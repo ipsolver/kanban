236bc019d0b7a91a87a36fbb5a38a765a297de22
ipsolver <vadimlisinskij44@gmail.com>
2026-02-05 14:31:32 +0200

    Added config module for project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service.js';
+import { BoardsController } from './boards/boards.controller';
+import { BoardsModule } from './boards/boards.module';
+import { BoardsService } from './boards/boards.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  }), BoardsModule],
+  controllers: [AppController, BoardsController],
+  providers: [AppService, PrismaService, BoardsService],
 })
 export class AppModule {}
