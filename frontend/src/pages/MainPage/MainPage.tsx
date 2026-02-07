7e44af51c1b35f8800ce72070131f6d0e408126e
ipsolver <vadimlisinskij44@gmail.com>
2026-02-07 20:42:10 +0200

    Output data from DB in MainPage

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,19 +1,35 @@
 import styles from './MainPage.module.css';
+import { useBoard } from '../../hooks/useBoard';
+import { useTasks } from '../../hooks/useTasks';
 import { Board } from '../../components/Board/Board';
+import { useState } from 'react';
 
 export const MainPage = () => {
-    return (
+  const [inputId, setInputId] = useState('');
+  const [boardId, setBoardId] = useState<string | null>(null);
+
+  const { board } = useBoard(boardId);
+  const { tasks } = useTasks(boardId);
+
+  return (
     <div className={styles.page}>
-        <div className={styles.topBar}>
-            <input
-                placeholder="Write here ID of board"
-                className={styles.input}
-            />
-        <button>Load</button>
+      <div className={styles.topBar}>
+        <input
+          placeholder="Write here ID of board"
+          className={styles.input}
+          value={inputId}
+          onChange={e => setInputId(e.target.value)}
+        />
+        <button onClick={() => setBoardId(inputId)}>Load</button>
         <button>New</button>
       </div>
 
-      <Board />
+      {board && (
+        <Board
+          board={board}
+          tasks={tasks}
+        />
+      )}
     </div>
   );
 };
