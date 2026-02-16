8ac7cbb5968e81d46d66a2c6f5b3d15f28cf6774
ipsolver <vadimlisinskij44@gmail.com>
2026-02-16 16:53:11 +0200

    some changes

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL;
 
-console.log("ENV:", API_URL);
-
 export const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
