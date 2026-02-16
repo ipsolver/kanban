62f735f96b49809d7efccafeb0b0ef238084c6bb
ipsolver <vadimlisinskij44@gmail.com>
2026-02-16 16:08:26 +0200

    Changed connection for deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',
+      'https://kanban-seven-sigma.vercel.app'
+    ],
     credentials: true,
   });
   
