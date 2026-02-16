6a50f76ce8b7d0cb8873a3617222c97f0c5763b3
ipsolver <vadimlisinskij44@gmail.com>
2026-02-16 16:26:44 +0200

    try another url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://kanban-production-337f.up.railway.app',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
