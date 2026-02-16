897d543b924cf30af994790b25b26eba6b6f04d2
ipsolver <vadimlisinskij44@gmail.com>
2026-02-16 16:38:15 +0200

    Check some path

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+const API_URL = import.meta.env.VITE_API_URL;
+
+console.log("ENV:", API_URL);
 
 export const api = axios.create({
   baseURL: 'https://kanban-production-337f.up.railway.app',
